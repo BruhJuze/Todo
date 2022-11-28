1ffc0bc59a1136773bb8a316732dec0a1237364e
Mikail Erdogan <mikailerdogan19@gmail.com>
2022-11-28 08:27:16 +0100

    clearable

--- src/js/main.js
+++ src/js/main.js
@@ -3,19 +3,21 @@ let myDiv = document.getElementById("mainDiv");
 let newDiv = document.createElement("div");
 newDiv.id = "mainDiv";
 
-window.addEventListener('load', (event) =>{
+window.addEventListener("load", (event) => {
+  myforloop();
+});
+window.startDrag("load", (event) => {
   myforloop();
 });
 
 myBody.style.backgroundColor = "#666633";
-
 
 myDiv.style.backgroundColor = "yellow";
 myDiv.style.height = "auto";
 myDiv.style.width = "375px";
 myDiv.style.padding = "10px";
 myDiv.style.position = "absolute";
-myDiv.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px" ;
+myDiv.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
 myDiv.style.overflow = "hidden";
 
 newDiv.style.backgroundColor = "yellow";
@@ -23,9 +25,8 @@ newDiv.style.height = "auto";
 newDiv.style.width = "375px";
 newDiv.style.padding = "10px";
 newDiv.style.position = "absolute";
-newDiv.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px" ;
+newDiv.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
 newDiv.style.overflow = "hidden";
-
 
 class Todo {
   constructor(what, when, isDone, title) {
@@ -36,11 +37,11 @@ class Todo {
   }
 }
 
-let todoTitle = new Todo("","","","Example")
+let todoTitle = new Todo("", "", "", "Example");
 let firstThing = new Todo("Wake up", "06:00", true);
-let secondThing = new Todo("Shower", "06:25", true);
-let thirdThing = new Todo("Eat breakfast", "06:45", true);
-let fourthThing = new Todo("Go to school", "07:15", true);
+let secondThing = new Todo("Shower", "06:25", false);
+let thirdThing = new Todo("Eat breakfast", "06:45", false);
+let fourthThing = new Todo("Go to school", "07:15", false);
 let fithThing = new Todo("Come home & grind with your boys", "~16:00", false);
 let sixthThing = new Todo("sleep and repeat at schooldays", "02:00", false);
 //let testThing = new Todo("test", "test", checkTest);
@@ -51,7 +52,7 @@ let todoschoolday = [
   thirdThing,
   fourthThing,
   fithThing,
-  sixthThing
+  sixthThing,
 ];
 
 let clearCheckThing = [];
@@ -70,7 +71,7 @@ console.log(objTodo);
 let titleEl = document.createElement("h1");
 let ul = document.createElement("ul");
 let container = document.createElement("div");
-let clearBtn = document.createElement("button"); 
+let clearBtn = document.createElement("button");
 let clearCheckedBtn = document.createElement("button");
 let myLabel = document.createElement("label");
 let firstround = 0;
@@ -82,37 +83,40 @@ myBody.appendChild(addBtn);
 container.className = "container";
 
 function functionClear() {
-  if (confirm("Are you sure you want to delete this note?")){
+  if (confirm("Are you sure you want to delete this note?")) {
     myDiv.remove();
   }
-  
-  
 }
 
-function altCheck(){
-
-  if(myCheck.check == true){
+function altCheck() {
+  if (myCheck.check == true) {
     altChecked = true;
-  }
-  else {
+  } else {
     altChecked = false;
   }
 }
 
-function addNote(){
-  
+function addNote() {
   //let newDiv = document.createElement("div");
   myBody.appendChild(newDiv);
+  let tbxName = document.createElement("input");
+  let tbxWhen = document.createElement("input");
+  let btnSubmit = document.createElement("button");
+  btnSubmit.textContent = "Add";
+  newDiv.appendChild(tbxName);
+  newDiv.appendChild(tbxWhen);
+  newDiv.appendChild(btnSubmit);
+  dragElement(newDiv);
 }
 
-clearBtn.addEventListener('click', functionClear);
-clearCheckedBtn.addEventListener('click', myforloop);
-addBtn.addEventListener('click', addNote);
-
+clearBtn.addEventListener("click", functionClear);
+clearCheckedBtn.addEventListener("click", myforloop);
+addBtn.addEventListener("click", addNote);
+addBtn.addEventListener("click", startDrag);
 
 titleEl.style.paddingLeft = "25px";
 clearBtn.style.float = "right";
-clearBtn.textContent = 'X';
+clearBtn.textContent = "X";
 clearBtn.style.margin = "5px";
 clearCheckedBtn.style.float = "right";
 clearCheckedBtn.textContent = "Clear checked";
@@ -123,59 +127,80 @@ addBtn.style.width = "60px";
 addBtn.style.margin = "20px";
 addBtn.style.fontSize = "xxx-large";
 
-
 titleEl.innerHTML = todoTitle.title;
 container.appendChild(titleEl);
 
 myDiv.appendChild(clearBtn);
 
-function myforloop(){ 
+function myforloop() {
+  myLabel.innerHTML = "";
+   
+  for (let i = 0; i < tlength; i++) {
+    let li = document.createElement("li");
+    let myCheckbox = document.createElement("input");
+    //let altCheck = false;
 
-  myLabel.remove();
-  
-for (let i = 0; i < tlength; i++) {
-  let li = document.createElement("li");
-  let myCheckbox = document.createElement("input");
+    myCheckbox.setAttribute("type", "checkbox");
 
-  myCheckbox.setAttribute("type", "checkbox");
+    li.className = "todo_li";
+    myCheckbox.className = "todo_check";
+    myLabel.for = "my_Label";
+    myCheckbox.id = "my_check";
+    myLabel.id = "my_check";
 
+    if (todoschoolday[i].when == "") {
+      li.innerHTML = todoschoolday[i].what + todoschoolday[i].when;
+    } else {
+      li.innerHTML = todoschoolday[i].what + " at " + todoschoolday[i].when;
+    }
 
-  li.className = "todo_li";
-  myCheckbox.className = "todo_check";
-  myLabel.for = "my_Label";
-  myCheckbox.id = "my_check";
-  myLabel.id = "my_check";
-  
+    myCheckbox.innerHTML = myCheckbox.checked = todoschoolday[i].isDone;
 
-li.innerHTML = todoschoolday[i].what + " at " + todoschoolday[i].when;
+    container.appendChild(ul);
+    ul.appendChild(myLabel);
+    li.appendChild(myCheckbox);
+    myLabel.appendChild(li);
 
-myCheckbox.innerHTML = myCheckbox.checked = todoschoolday[i].isDone;
+    // console.log(altCheck);
 
-  container.appendChild(ul);
-  ul.appendChild(myLabel);
-  li.appendChild(myCheckbox);
-  myLabel.appendChild(li);
-  
+    if (myCheckbox.checked == true && firstround > 0) {
+      li.remove();
+    }
 
+    myCheckbox.addEventListener("click", function () {
+      if (this.checked) {
+        altCheck = true;
+        todoschoolday[i].isDone = true;
+      } else {
+        altCheck = false;
+        todoschoolday[i].isDone = false;
+      }
+    });
+    console.log(myCheckbox.checked);
+  }
   myDiv.appendChild(container);
-
-  
+  console.log(clearCheckThing);
+  myDiv.appendChild(clearCheckedBtn); 
+  if(firstround == 0){
+  firstround++; 
+}
 }
 
-
-
-
-
-dragElement(myDiv, newDiv);
+function startDrag() {
+  dragElement(myDiv);
+}
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
     /* if present, the header is where you move the DIV from:*/
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
     /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown; 
+    elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
@@ -186,7 +211,7 @@ function dragElement(elmnt) {
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag; 
+    document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
@@ -194,25 +219,22 @@ function dragElement(elmnt) {
     e.preventDefault();
     // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY; 
+    pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
     //give shadow:
-    myDiv.style.boxShadow = "rgba(0, 0, 0, 10) 0px 10px 20px" ;
+    myDiv.style.boxShadow = "rgba(0, 0, 0, 10) 0px 10px 20px";
+    newDiv.style.boxShadow = "rgba(0, 0, 0, 10) 0px 10px 20px";
   }
 
   function closeDragElement() {
     /* stop moving when mouse button is released:*/
     document.onmousemove = null;
     //remove shadow:
-    myDiv.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px" ;
+    myDiv.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
+    newDiv.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
   }
 }
-
-
-
-
-
